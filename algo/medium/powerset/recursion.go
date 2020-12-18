e375d17fe2762fd7f02c2657d5f0bb6d12a82952
James Yang <james_yang@htc.com>
2020-12-18 14:16:40 +0800

    UPD. update recursion version for powerset

--- algo/medium/powerset/recursion.go
+++ algo/medium/powerset/recursion.go
@@ -1,17 +1,28 @@
 package powerset
 
 func helper(array []int, index int) [][]int {
-	var res [][]int
 	if index < 0 {
-		res = [][]int{{}}
+		return [][]int{{}}
 	}
 
-	// ....
+	subsets := helper(array, index-1)
+	res := [][]int{}
+	// deep copy
+	for j := range subsets {
+		res = append(res, subsets[j])
+	}
 
+	for i := 0; i < len(subsets); i++ {
+		set := append([]int{}, subsets[i]...)
+		newsets := append(set, array[index])
+		res = append(res, newsets)
+	}
 	return res
 }
 
 // Recursion ...
+// T O(n*2^n) 1, 2, 4, 8, 16...
+// S O(n*2^2) stack space use as n, but tmp array grow as large as n*2^n
 func Recursion(array []int) [][]int {
-	helper(array, len(array)-1)
+	return helper(array, len(array)-1)
 }
