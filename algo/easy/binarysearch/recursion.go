0eb7ba833b9a371f1fbcdc0735f47670e504eebc
James Yang <james_yang@htc.com>
2020-12-21 12:01:51 +0800

    UPD. update recursion for binary search

--- algo/easy/binarysearch/recursion.go
+++ algo/easy/binarysearch/recursion.go
@@ -1,8 +1,24 @@
 package binarysearch
 
+func helper(sorted []int, i, j, target int) int {
+	if i > j {
+		return -1
+	}
+
+	mid := (i + j) / 2
+
+	if sorted[mid] == target {
+		return mid
+	}
+
+	if sorted[mid] > target {
+		return helper(sorted, i, mid-1, target)
+	}
+
+	return helper(sorted, mid+1, j, target)
+}
+
 // Recursion ...
 func Recursion(sorted []int, target int) int {
-	res := -1
-
-	return res
+	return helper(sorted, 0, len(sorted)-1, target)
 }
